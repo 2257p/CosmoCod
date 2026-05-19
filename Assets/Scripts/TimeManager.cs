74a82e3049189392719a9db61f4d2a41e1233c77
zvincentt <zvincentzeng@gmail.com>
2026-05-19 14:24:12 -0400

    Clock (broken)

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public const int hoursInDay = 24, minutesInHour = 60;
+
+    public float dayDuration = 30f;
+
+    float totalTime = 0;
+    float currentTime = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        currentTime = totalTime % dayDuration;
+    }
+
+    public float GetHour()
+    {
+        return currentTime * hoursInDay / dayDuration;
+    }
+
+    public float GetMinutes()
+    {
+        return (currentTime * hoursInDay * minutesInHour / dayDuration)%minutesInHour;
+    }
+
+    public string Clock24Hour()
+    {
+        return Mathf.FloorToInt(GetHour()).ToString("00") + ":" + Mathf.FloorToInt(GetMinutes()).ToString("00");
+    }
+
+}
