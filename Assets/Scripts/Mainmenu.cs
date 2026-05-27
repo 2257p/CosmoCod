19ee9178fe4115470bdab78c1fd4ee4dc886cde7
Zellindorz <matin.hadidi26@ycdsbk12.ca>
2026-05-27 16:25:47 -0400

    Small edit

--- Assets/Scripts/Mainmenu.cs
+++ Assets/Scripts/Mainmenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene("TestScene");
+        SceneManager.LoadScene("CC Planet");
     }
 
     public void Option()
